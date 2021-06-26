78cdf8742cb49a84a4d0e375bbb5e03442119dca
tiago-1991 <53984116+tiago-1991@users.noreply.github.com>
2021-06-26 06:20:11 -0300

    Primeiro Comite Frond End

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+import './App.css';
+import Imagem from './Componentes/imagem';
+
+function App() {
+  return (
+    <Imagem />
+  );
+}
+
+export default App;
---
+++ src/Componentes/imagem.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Imagem extends Component {
+    render() {
+        return (
+            <div>  
+
+                <img src="disco.png" alt="Foto dos Discos" />          
+               
+            </div>
+            
+    
+    
+        );
+    }
+}
+
+export default Imagem;
